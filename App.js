03b7f4da9e70db0f2f835df5c8d98f9ccdedf532
David Poslušný <david.poslusny@email.cz>
2023-02-13 18:40:01 +0100

    redux setup + home component

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import Home from './screens/Home';
+import { store } from './store';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Uber clone build.</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <Home/>
+    </Provider>
   );
 }
 
---
+++ screens/Home.js
@@ -0,0 +1,39 @@
+import { 
+  Image, 
+  Platform, 
+  SafeAreaView, 
+  StyleSheet, 
+  Text, 
+  View
+} from 'react-native'
+import React from 'react'
+import tw from 'tailwind-react-native-classnames'
+
+
+const Home = () => {
+  return (
+    <SafeAreaView style={[tw`bg-white h-full`, styles.container]}>
+      <View style={tw`p-5`}>
+        <Image
+          style={{
+            width: 100,
+            height: 100,
+            resizeMode: 'contain'
+          }}
+          source={{
+            uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Uber_logo_2018.svg/2560px-Uber_logo_2018.svg.png"
+          }}
+        />
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default Home
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: Platform.OS === 'android' ? 25 : 0
+    }
+})
